40ea8cfbda98494c8ae59d155ca5c4007a627109
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-08-18 14:31:29 -0500

    FEAT(Intro): added first text

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 
-class Intro extends Component {
+export default class Intro extends Component {
   constructor() {
     super()
-    state: {
-      displayText: ""
+    this.state = {
+      displayText: "hi"
     }
   }
 
   render() {
     return (
       <div>
-
+        <h1>{this.state.displayText}</h1>
       </div>
     )
   }
