532fb9b9f67490631042c79ca3b3106a43b88391
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-08-18 14:28:01 -0500

    FEAT(intro): added Intro component

---
+++ src/components/Intro.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Intro extends Component {
+  constructor() {
+    super()
+    state: {
+      displayText: ""
+    }
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
