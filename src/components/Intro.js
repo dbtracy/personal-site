22d7a51060189032def15122f1268339a6ac1efb
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-08-18 14:38:26 -0500

    FEAT(Intro): added timeout intro text; needs debugging

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,11 +4,21 @@ export default class Intro extends Component {
   constructor() {
     super()
     this.state = {
-      displayText: "hi"
+      displayText: ""
     }
   }
 
   render() {
+    setTimeout(() => {
+      this.setState({ displayText: "hi" })
+      setTimeout(() => {
+        this.setState({ displayText: "my name's Dan" })
+        setTimeout(() => {
+          this.setState({ displayText: "and this is my website" })
+        }, 3020);
+      }, 3010);
+    }, 3000);
+
     return (
       <div>
         <h1>{this.state.displayText}</h1>
