a501b7ee6a4541bc917fa29d2751116cfe50a747
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-09-01 14:28:20 -0400

    FEAT(Bio): added Bio component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Router } from 'react-router-dom'
 import './App.css';
-// import Home from './components/Home'
+
+import history from './history'
 import Navbar from './components/Navbar'
 import Routes from './components/Routes'
 
 function App() {
   return (
     <div className="App">
-      <Router history={history}>
-        <Navbar />
-        <Routes />
+      <Router history={history} >
+        <div className="App-header">
+          <Navbar />
+          <Routes />
+        </div>
       </Router>
     </div>
   );
---
+++ src/components/Bio.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Bio extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
