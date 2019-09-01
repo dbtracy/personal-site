c89083734c21f4360fbc4444c0f0b87d875b6a6d
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-09-01 13:27:38 -0400

    FEAT(index.js): added basic routes config

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,15 +9,21 @@ export default class Intro extends Component {
   }
 
   render() {
-    setTimeout(() => {
-      this.setState({ displayText: "hi" })
+    const introText = ['hi', 'my name\'s Dan', 'and this is my website']
+    for (let words of introText) {
       setTimeout(() => {
-        this.setState({ displayText: "my name's Dan" })
-        setTimeout(() => {
-          this.setState({ displayText: "and this is my website" })
-        }, 3020);
-      }, 3010);
-    }, 3000);
+        this.setState({ displayText: words })
+      }, 3000);
+    }
+    // setTimeout(() => {
+    //   this.setState({ displayText: "hi" })
+    //   setTimeout(() => {
+    //     this.setState({ displayText: "my name's Dan" })
+    //     setTimeout(() => {
+    //       this.setState({ displayText: "and this is my website" })
+    //     }, 3020);
+    //   }, 3010);
+    // }, 3000);
 
     return (
       <div>
