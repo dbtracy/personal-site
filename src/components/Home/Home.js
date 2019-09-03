efd15a272bb4c7d77acd5e1bd5ea2be617e337fb
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-09-03 08:37:23 -0400

    FEAT(AboutMe): created basic AboutMe component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
 import './Home.css'
 import pic from './headshot.square.png'
+import AboutMe from '../AboutMe/AboutMe'
 
 export default class Home extends Component {
   render() {
     return (
-      <div className="dan-basic-info">
-        <img className="headshot" src={pic} alt="dt-pic" />
-        <h1>Dan Tracy</h1>
-        <div>
-          <p>Full stack web developer</p>
-          <p>Musician</p>
-          <p>Tree-hugger</p>
+      <div>
+        <div className="dan-basic-info">
+          <img className="headshot" src={pic} alt="dt-pic" />
+          <h1>Dan Tracy</h1>
+          <div>
+            <p>Full stack web developer</p>
+            <p>Musician</p>
+            <p>Tree-hugger</p>
+          </div>
         </div>
+        <AboutMe />
       </div>
     )
   }
