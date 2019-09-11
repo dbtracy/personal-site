5cf60e16c08e8309eca9f6ab34c17381bee3fc76
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-09-11 12:29:32 -0400

    FIX(Home): deleted extraneous divs and CSS class

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,11 +7,9 @@ export default function Home() {
   return (
     <div>
       <div className="dan-basic-info">
-        {/* <div> */}
         <div className="headshot-border">
           <img className="headshot" src={pic} alt="dt-pic" />
         </div>
-        {/* </div> */}
         <h1>Dan Tracy</h1>
         <div>
           <p>Full stack web developer</p>
