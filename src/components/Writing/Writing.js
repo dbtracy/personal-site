c00ba29a64cd45a1d7e1f2bb4690a1dc8e6e14ac
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-21 20:14:15 -0400

    FEAT(Writing): basic writing page up and running

--- src/components/Writing/Writing.js
+++ src/components/Writing/Writing.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 
-export default class Bio extends Component {
+import './Writing.css'
+
+export default class Writing extends Component {
   render() {
     return (
-      <div >
-        <h1>This is my writing!</h1>
+      <div>
+        <h1>Check out my writing!</h1>
+        <div className="article-list">
+          <a className="spec-project" href="https://medium.com/@daniel.benjamin.tracy/resolving-cors-conflicts-with-the-arcgis-javascript-api-30b35e6b762a?source=your_stories_page---------------------------">Resolving CORS Conflicts with the ArcGIS JavaScript API</a>
+          <a className="spec-project" href="https://medium.com/@daniel.benjamin.tracy/origins-origin-story-the-history-of-web-content-origin-733d76a8f8fd?source=your_stories_page---------------------------">Origin's Origin Story: the History of Web Content Origin</a>
+          <a className="spec-project" href="https://medium.com/@daniel.benjamin.tracy/why-you-yes-you-should-learn-to-code-6969cb27c357?source=your_stories_page---------------------------">Why You (yes, you) Should Learn to Code</a>
+        </div>
       </div>
     )
   }
